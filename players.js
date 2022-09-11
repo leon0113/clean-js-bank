86037de9e67a4ebe71faa303f839ce30fa66ee33
leon0113 <tahjib.leon399@gmail.com>
2022-09-12 04:44:53 +0600

    class style

---
+++ players.js
@@ -0,0 +1,11 @@
+const players = document.getElementsByClassName('player');
+for (const player of players) {
+    console.log(player);
+    player.style.border = '3px solid red';
+    player.style.margin = '10px';
+    player.style.padding = '10px';
+}
+
+function addplayer() {
+
+}
